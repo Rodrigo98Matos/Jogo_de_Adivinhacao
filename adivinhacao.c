baaf60fb4337137d180072c081b4ddfc64c74838
Rodrigo Matos <rodrigo98matos@gmail.com>
2021-05-28 20:17:50 -0300

    add logica no jogo

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,5 +6,16 @@ int main(void){
     printf("Qual é o seu palpite?\t");
     scanf("%d", &chute);
     printf("Seu palpite foi:\t%d", chute);
+    int acertou = chute == numerosecreto;
+    if(acertou){
+        printf("\033[32m\nParabéns, Você acertou!\033[m\n");
+    }else{
+        int maior = chute > numerosecreto;
+        if(maior){
+            printf("\033[33m\nSeu Chute foi maior do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
+        }else{
+            printf("\033[33m\nSeu chute foi menor do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
+        };
+    };
     return 0;
 };

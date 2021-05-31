08478a035dc022cf338edad11cac849d65f53f6d
Rodrigo Matos <rodrigo98matos@gmail.com>
2021-05-30 21:41:39 -0300

    Add loops

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#define NUMERO_DE_TENTATIVAS 3
 
 int main(void){
-    int chute, numerosecreto = 42;
-    printf("Bem-Vindo ao Jogo de Adivinhação!\n");
-    printf("Qual é o seu palpite?\t");
-    scanf("%d", &chute);
-    printf("Seu palpite foi:\t%d", chute);
-    int acertou = chute == numerosecreto;
-    if(acertou){
-        printf("\033[32m\nParabéns, Você acertou!\033[m\n");
-    }else{
-        int maior = chute > numerosecreto;
-        if(maior){
-            printf("\033[33m\nSeu Chute foi maior do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
-        }else{
-            printf("\033[33m\nSeu chute foi menor do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
+    int chute;
+    unsigned int numerosecreto = 42;
+    printf("=================================================\n");
+    printf("\tBem-Vindo ao Jogo de Adivinhação!\n");
+    printf("=================================================\n");
+    for (unsigned int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
+        printf("Qual é o seu %d° palpite?\t", i);
+        scanf("%d", &chute);
+        printf("Seu %do. palpite foi:\t%d", i, chute);
+        short int acertou = chute == numerosecreto;
+        short int maior = chute > numerosecreto;
+        unsigned int invalido = chute < 0;
+        if (invalido){
+            printf("\033[31mDigite um número positivo!\033[m\n");
+            i--;
+        } else if (acertou) {
+            printf("\033[32m\nParabéns, Você acertou!\033[m\n");
+            break;
+        } else if(maior) {
+                printf("\033[33m\nSeu Chute foi maior do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
+            } else {
+                printf("\033[33m\nSeu chute foi menor do que o número secreto! Mas não desanime, jogue novamente!\033[m\n");
+            };
         };
-    };
+    printf("---------------------------\n");
+    printf("\tFim de Jogo!\n");
+    printf("---------------------------\n");
     return 0;
 };
